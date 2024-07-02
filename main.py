689f9af9e9e3eaf6f9781e0652321ae97bcc56b5
mildredymorales <mmoralesparedes@g.hmc.edu>
2024-07-02 08:44:20 -0400

    fix indent

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from tasks import BiologicalAnalysisTask
 
 
 class BiologicalAnalysisCrew:
-  def __init__(self, var1, var2):
+    def __init__(self, var1, var2):
         self.var1 = var1
         self.var2 = var2
 
